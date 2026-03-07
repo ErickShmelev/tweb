e7fc418e7c938442ebc86f07038a0bd913e089c7
Your Name <your-email@example.com>
2026-03-07 21:14:48 +0300

    Fix lint errors in ripple.ts

--- src/components/ripple.ts
+++ src/components/ripple.ts
@@ -4,15 +4,15 @@
  * https://github.com/morethanwords/tweb/blob/master/LICENSE
  */
 
-import findUpClassName from "@helpers/dom/findUpClassName";
-import sequentialDom from "@helpers/sequentialDom";
-import IS_TOUCH_SUPPORTED from "@environment/touchSupport";
-import findUpAsChild from "@helpers/dom/findUpAsChild";
-import { fastRaf } from "@helpers/schedulers";
-import liteMode from "@helpers/liteMode";
-import { Accessor, createRenderEffect, onCleanup } from "solid-js";
+import findUpClassName from '@helpers/dom/findUpClassName';
+import sequentialDom from '@helpers/sequentialDom';
+import IS_TOUCH_SUPPORTED from '@environment/touchSupport';
+import findUpAsChild from '@helpers/dom/findUpAsChild';
+import {fastRaf} from '@helpers/schedulers';
+import liteMode from '@helpers/liteMode';
+import {Accessor, createRenderEffect, onCleanup} from 'solid-js';
 
-declare module "solid-js" {
+declare module 'solid-js' {
   namespace JSX {
     interface Directives {
       /**
@@ -26,32 +26,33 @@ declare module "solid-js" {
 let rippleClickId = 0;
 function _ripple(
   elem: HTMLElement,
-  prepend: boolean | "no" = true,
+  prepend: boolean | 'no' = true,
   callback: (id: number) => Promise<boolean | void> = () => Promise.resolve(),
   onEnd: (id: number) => void = null,
   attachListenerTo = elem,
 ) {
   // return;
-  if (elem.querySelector(".c-ripple")) return;
-  elem.classList.add("rp");
+  console.log('openchat');
+  if (elem.querySelector('.c-ripple')) return;
+  elem.classList.add('rp');
 
-  const r = document.createElement("div");
-  r.classList.add("c-ripple");
+  const r = document.createElement('div');
+  r.classList.add('c-ripple');
 
-  const isSquare = elem.classList.contains("rp-square");
+  const isSquare = elem.classList.contains('rp-square');
   if (isSquare) {
-    r.classList.add("is-square");
+    r.classList.add('is-square');
   }
 
-  if (prepend !== "no") {
-    elem[prepend ? "prepend" : "append"](r);
+  if (prepend !== 'no') {
+    elem[prepend ? 'prepend' : 'append'](r);
   }
 
   let handler: () => void;
   // let animationEndPromise: Promise<number>;
   const drawRipple = (clientX: number, clientY: number) => {
     const startTime = Date.now();
-    const circle = document.createElement("div");
+    const circle = document.createElement('div');
 
     const clickId = rippleClickId++;
 
@@ -64,8 +65,8 @@ function _ripple(
         ? 0.3
         : +window
             .getComputedStyle(r)
-            .getPropertyValue("--ripple-duration")
-            .replace("s", "")) * 1000;
+            .getPropertyValue('--ripple-duration')
+            .replace('s', '')) * 1000;
     // console.log('ripple duration', duration);
 
     const _handler = (handler = () => {
@@ -86,19 +87,19 @@ function _ripple(
       if (elapsedTime < duration) {
         const delay = Math.max(duration - elapsedTime, duration / 2);
         setTimeout(
-          () => circle.classList.add("hiding"),
+          () => circle.classList.add('hiding'),
           Math.max(delay - duration / 2, 0),
         );
 
         setTimeout(cb, delay);
       } else {
-        circle.classList.add("hiding");
+        circle.classList.add('hiding');
         setTimeout(cb, duration / 2);
       }
 
       if (!IS_TOUCH_SUPPORTED) {
-        window.removeEventListener("contextmenu", handler);
-        window.removeEventListener("mousemove", handler);
+        window.removeEventListener('contextmenu', handler);
+        window.removeEventListener('mousemove', handler);
       }
 
       handler = null;
@@ -127,7 +128,7 @@ function _ripple(
       }
 
       const rect = r.getBoundingClientRect();
-      circle.classList.add("c-ripple__circle");
+      circle.classList.add('c-ripple__circle');
 
       const clickX = clientX - rect.left;
       const clickY = clientY - rect.top;
@@ -144,10 +145,10 @@ function _ripple(
 
       // console.log('ripple click', offsetFromCenter, size, clickX, clickY);
 
-      circle.style.width = circle.style.height = size + "px";
-      circle.style.left = x + "px";
-      circle.style.top = y + "px";
-      circle.style.opacity = "0";
+      circle.style.width = circle.style.height = size + 'px';
+      circle.style.left = x + 'px';
+      circle.style.top = y + 'px';
+      circle.style.opacity = '0';
 
       // нижний код выполняется с задержкой
       /* animationEndPromise = new Promise((resolve) => {
@@ -166,7 +167,7 @@ function _ripple(
       r.append(circle);
 
       void circle.offsetWidth; // force reflow
-      circle.style.opacity = "";
+      circle.style.opacity = '';
 
       if (auto) {
         // window.addEventListener('mousemove', handler, {once: true, passive: true});
@@ -182,11 +183,11 @@ function _ripple(
   const isRippleUnneeded = (e: Event) => {
     return (
       e.target !== elem &&
-      (["BUTTON", "A"].includes((e.target as HTMLElement).tagName) ||
-        findUpClassName(e.target as HTMLElement, "c-ripple") !== r) &&
+      (['BUTTON', 'A'].includes((e.target as HTMLElement).tagName) ||
+        findUpClassName(e.target as HTMLElement, 'c-ripple') !== r) &&
       (attachListenerTo === elem ||
         !findUpAsChild(e.target as HTMLElement, attachListenerTo)) &&
-      !findUpClassName(e.target, "checkbox-field")
+      !findUpClassName(e.target, 'checkbox-field')
     );
   };
 
@@ -198,7 +199,7 @@ function _ripple(
     };
 
     const onTouchStart = (e: TouchEvent) => {
-      if (!liteMode.isAvailable("animations")) {
+      if (!liteMode.isAvailable('animations')) {
         return;
       }
 
@@ -212,26 +213,26 @@ function _ripple(
 
       const { clientX, clientY } = e.touches[0];
       drawRipple(clientX, clientY);
-      attachListenerTo.addEventListener("touchend", touchEnd, { once: true });
+      attachListenerTo.addEventListener('touchend', touchEnd, { once: true });
 
       window.addEventListener(
-        "touchmove",
+        'touchmove',
         (e) => {
           e.cancelBubble = true;
           e.stopPropagation();
           touchEnd();
-          attachListenerTo.removeEventListener("touchend", touchEnd);
+          attachListenerTo.removeEventListener('touchend', touchEnd);
         },
         { once: true },
       );
     };
 
-    attachListenerTo.addEventListener("touchstart", onTouchStart, {
+    attachListenerTo.addEventListener('touchstart', onTouchStart, {
       passive: true,
     });
     return {
       dispose: () =>
-        attachListenerTo.removeEventListener("touchstart", onTouchStart),
+        attachListenerTo.removeEventListener('touchstart', onTouchStart),
       element: r,
     };
   } else {
@@ -241,12 +242,12 @@ function _ripple(
         return;
       }
 
-      if (!liteMode.isAvailable("animations")) {
+      if (!liteMode.isAvailable('animations')) {
         return;
       }
       // console.log('ripple mousedown', e, e.target, findUpClassName(e.target as HTMLElement, 'c-ripple') === r);
 
-      if (attachListenerTo.dataset.ripple === "0" || isRippleUnneeded(e)) {
+      if (attachListenerTo.dataset.ripple === '0' || isRippleUnneeded(e)) {
         return;
       } else if (touchStartFired) {
         touchStartFired = false;
@@ -255,23 +256,22 @@ function _ripple(
 
       const { clientX, clientY } = e;
       drawRipple(clientX, clientY);
-      console.log("openchat");
-      window.addEventListener("mouseup", handler, {
+      window.addEventListener('mouseup', handler, {
         once: true,
         passive: true,
       });
-      window.addEventListener("contextmenu", handler, {
+      window.addEventListener('contextmenu', handler, {
         once: true,
         passive: true,
       });
     };
 
-    attachListenerTo.addEventListener("mousedown", onMouseDown, {
+    attachListenerTo.addEventListener('mousedown', onMouseDown, {
       passive: true,
     });
     return {
       dispose: () =>
-        attachListenerTo.removeEventListener("mousedown", onMouseDown),
+        attachListenerTo.removeEventListener('mousedown', onMouseDown),
       element: r,
     };
   }
@@ -280,7 +280,7 @@ function _ripple(
 export default function ripple(
   elem: HTMLElement,
   accessor?: Accessor<boolean>,
-  prepend?: boolean | "no",
+  prepend?: boolean | 'no',
 ) {
   if (accessor) {
     createRenderEffect(() => {
